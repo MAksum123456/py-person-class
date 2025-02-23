fe9e3b41057ff858978e74199d3859a8cd021ec4
Max <smolinskijmaksim1@gmail.com>
2025-02-23 13:57:52 +0100

    Fix 2

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
+from typing import List, Optional
+
 class Person:
     people = {}
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
-
-def create_person_list(people_data):
+def create_person_list(people_data: List[dict]) -> List[Person]:
     for person_dict in people_data:
-        Person(person_dict["name"], person_dict["age"])
-
-    for person_dict in people_data:
-        person = Person.people[person_dict["name"]]
+        person = Person(person_dict["name"], person_dict["age"])
 
         if "wife" in person_dict and person_dict["wife"] is not None:
             person.wife = Person.people[person_dict["wife"]]
             person.wife.husband = person
-
         elif "husband" in person_dict and person_dict["husband"] is not None:
             person.husband = Person.people[person_dict["husband"]]
             person.husband.wife = person
